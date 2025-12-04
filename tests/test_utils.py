fadfd64f0162720093d27cfe81d2f8520a6cf12e
Polehampton <edward.polehampton@stfc.ac.uk>
2025-12-04 16:06:06 +0000

    Add tests for new utils functions

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -226,3 +226,163 @@ def test_create_path_manager_default(pm, chains):
 
             assert Path(base_dir + "/data").exists()
             assert Path(base_dir + "/results").exists()
+
+@pytest.mark.parametrize(
+    "key, prefix, label_prefix, expected_result",
+    [
+        ("GSM_FgEoR_-1e0pp", "GSM_FgEoR_", None, "GSM -1%"),
+        ("GSM_FgEoR_-1e1pp", "GSM_FgEoR_", None, "GSM -10%"),
+        ("GSM_FgEoR_-1e-3pp", "GSM_FgEoR_", None, "GSM -0.001%"),
+        ("GSM_FgEoR_-1.123e0pp", "GSM_FgEoR_", None, "GSM -1.12%"),
+        ("GSM_FgEoR_1e0pp", "GSM_FgEoR_", None, "GSM +1%"),
+        ("GSM_FgEoR_1e0pp", "GSM_FgEoR_", None, "GSM +1%"),
+        ("GSM_FgEoR_-1e0pp", "GSM_FgEoR_", "MyPrefix", "MyPrefix -1%"),
+        ("GSM_FgEoR_-1e0pp", "MyPrefix_", None, None),
+        ("GSM_FgEoR_-1e0", "GSM_FgEoR_", None, None),
+        ("GSM_FgEoR_-abcpp", "GSM_FgEoR_", None, None),
+    ],
+)
+def test_pp_key_to_percent_label(key, prefix, label_prefix, expected_result):
+    """ Test to strip prefix from key to produce labels """
+
+    label = utils._pp_key_to_percent_label(key, prefix, label_prefix)
+    
+    assert label == expected_result
+
+
+def test_build_pp_groups_from_paths_default():
+    """Test with default input - i.e. reads from paths.yaml"""
+
+    prefixes = ["GSM_FgEoR_"]
+
+    expected_output = {
+        'GSM -10%': ['GSM_FgEoR_-1e1pp'],
+        'GSM -5%': ['GSM_FgEoR_-5e0pp'], 
+        'GSM -2%': ['GSM_FgEoR_-2e0pp'], 
+        'GSM -1%': ['GSM_FgEoR_-1e0pp'], 
+        'GSM -0.1%': ['GSM_FgEoR_-1e-1pp'], 
+        'GSM -0.01%': ['GSM_FgEoR_-1e-2pp'], 
+        'GSM -0.001%': ['GSM_FgEoR_-1e-3pp'], 
+        'GSM +0.001%': ['GSM_FgEoR_+1e-3pp'], 
+        'GSM +0.01%': ['GSM_FgEoR_+1e-2pp'], 
+        'GSM +0.1%': ['GSM_FgEoR_+1e-1pp'], 
+        'GSM +1%': ['GSM_FgEoR_+1e0pp'], 
+        'GSM +2%': ['GSM_FgEoR_+2e0pp'], 
+        'GSM +5%': ['GSM_FgEoR_+5e0pp'], 
+        'GSM +10%': ['GSM_FgEoR_+1e1pp'],
+    }
+
+    groups = utils.build_pp_groups_from_paths(prefixes)
+
+    assert groups == expected_output
+
+
+@pytest.mark.parametrize(
+    "prefixes, label_prefixes, expected_result",
+    [
+        (
+            ["Test1_"], 
+            {"Test1_": "T1", "Test2_": "T2"}, 
+            {
+                'T1 -0.001%': ['Test1_-1e-3pp'],
+                'T1 -0.01%': ['Test1_-1e-2pp'],
+                'T1 -0.1%': ['Test1_-1e-1pp'],
+                'T1 -1%': ['Test1_-1e0pp'],
+            }
+        ),
+        (
+            ["Test2_"], 
+            {"Test1_": "T1", "Test2_": "T2"}, 
+            {
+                'T2 -2%': ['Test2_-2e0pp'],
+                'T2 -5%': ['Test2_-5e0pp'],
+                'T2 -10%': ['Test2_-1e1pp'],
+            }
+        ),
+        (
+            ["Test2_"], 
+            {"Dummy": "something"}, 
+            {
+                'Test2 -2%': ['Test2_-2e0pp'],
+                'Test2 -5%': ['Test2_-5e0pp'],
+                'Test2 -10%': ['Test2_-1e1pp'],
+            }
+        ),
+        (
+            ["Test2_"], 
+            None, 
+            {
+                'Test2 -2%': ['Test2_-2e0pp'],
+                'Test2 -5%': ['Test2_-5e0pp'],
+                'Test2 -10%': ['Test2_-1e1pp'],
+            }
+        ),
+        (
+            ["Test2_"], 
+            None, 
+            {
+                'Test2 -2%': ['Test2_-2e0pp'],
+                'Test2 -5%': ['Test2_-5e0pp'],
+                'Test2 -10%': ['Test2_-1e1pp'],
+            }
+        ),
+        (
+            ["Test3_"], 
+            {"Test1_": "T1", "Test2_": "T2"}, 
+            {}
+        ),
+        (
+            ["Test4_"], 
+            {"Test1_": "T1", "Test2_": "T2"}, 
+            {}
+        ),
+    ],
+)
+def test_build_pp_groups_from_paths_with_input(prefixes, label_prefixes, expected_result):
+    """Test with input yaml file"""
+
+    with tempfile.NamedTemporaryFile(mode="w+t") as yaml_file:
+        yaml_file.writelines(
+            [
+                "Test1_-1e-3pp: test/directory1a/\n",
+                "Test1_-1e-2pp: test/directory1b/\n",
+                "Test1_-1e-1pp: test/directory1c/\n",
+                "Test1_-1e0pp: test/directory1d/\n",
+                "Test2_-2e0pp: test/directory2a/\n",
+                "Test2_-5e0pp: test/directory2b/\n",
+                "Test2_-1e1pp: test/directory2c/\n",
+                "Test3_-ee1pp: test/directory3/",
+            ]
+        )
+   
+        yaml_file.seek(0)
+
+        groups = utils.build_pp_groups_from_paths(
+            prefixes, 
+            custom_paths_file=yaml_file.name,
+            label_prefixes=label_prefixes,
+        )
+
+        yaml_file.close()
+
+        assert groups == expected_result
+
+
+def test_build_group_labels():
+    """Test build group labels"""
+
+    groups = {
+        "key1": ["value1"],
+        "key2": ["value2a", "value2b"],
+        "key3": ["value3"],
+    }
+
+    expected_result = {
+        "key1": "key1",
+        "key2": "key2",
+        "key3": "key3",
+    }
+
+    labels = utils.build_group_labels(groups)
+
+    assert labels == expected_result

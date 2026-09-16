ee4c355396714846dab139bf5a350fe5728ef7df
PSims <6097708+PSims@users.noreply.github.com>
2026-09-16 09:17:01 +0000

    Use declared dependencies in API documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,20 +41,7 @@ autoclass_content = "both"
 
 autodoc_mock_imports = [
     "bayeseor",
-    "anesthetic",
-    "matplotlib",
-    "pandas",
     "IPython",
-    "yaml",
-    "tqdm",
-    "lmfit",
-    "numpy",
-    "scipy",
-    "scipy.stats",
-    "ruamel",
-    "ruamel.yaml",
-    "rich",
-    "pyuvdata",
 ]
 
 templates_path = ["_templates"]
